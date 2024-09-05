7a7e011d339b634f6f2cfb9bc4c3a4302e5446d4
Ilan Bernstein <59799324+AncientBison@users.noreply.github.com>
2024-09-05 16:13:14 +0000

    Add redis url

--- lib/redis.ts
+++ lib/redis.ts
@@ -1,7 +1,9 @@
 import { createClient } from "redis";
 
 export async function getClient() {
-  const client = await createClient()
+  const client = await createClient({
+    url: process.env.REDIS_URL,
+  })
     .on("error", (err) => () => {
       if (process.env.NODE_ENV === "development") {
         console.error("Redis Client Error", err);
