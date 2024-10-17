05d6513035d316aedee13dfd37774f2d360d9c0f
Ancient Bison <ilansbernstein@gmail.com>
2024-10-17 18:58:31 -0400

    Remove unnecessary file

--- lib/isPayloadOfType.ts
+++
@@ -1,8 +0,0 @@
-import { MessageType, TypedMessage } from "@type/message";
-
-export function isPayloadOfType<T extends MessageType>(
-  message: any,
-  type: T,
-): message is TypedMessage<T> {
-  return message.type !== undefined && message.type === type;
-}
